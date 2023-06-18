4ac9db0093089e77fc857f0569e0048c5f43f914
Shaunak Sensarma <shaunak.sensarma@hpe.com>
2023-06-18 15:19:05 +0530

    database setup

---
+++ controllers/cart.go
@@ -0,0 +1,27 @@
+// (c) Copyright 2023, Shaunak Sensarma.
+
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AddToCart() gin.Handler {
+
+}
+
+func RemoveItem() gin.HandlerFunc {
+
+}
+
+func GetItemFromCart() gin.HandlerFunc {
+
+}
+
+func BuyFromCart() gin.HandlerFunc {
+
+}
+
+func InstantBuy() gin.HandlerFunc {
+
+}
---
+++ database/cart.go
@@ -0,0 +1,33 @@
+// (c) Copyright 2023, Shaunak Sensarma.
+
+package database
+
+import (
+	"errors"
+)
+
+var (
+	ErrCantFindProduct    = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdNotValid     = errors.New("this user is not valid")
+	ErrCantUpdateUser     = errors.New("cannot add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("cannot remove this item from the cart")
+	ErrCantGetItem        = errors.New("was unable to get item from the cart")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveItemFromCart() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
